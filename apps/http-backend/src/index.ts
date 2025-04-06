d87c751f32c1759d367afc641a7b925febedae2c
Yash Khare <khareyash05@gmail.com>
2025-04-06 23:19:21 +0530

    feat: zod schema
    Signed-off-by: Yash Khare <khareyash05@gmail.com>

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,26 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
+import {CreateRoomSchema, CreateUserSchema, SignInSchema} from '@repo/common/types';
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.status(400).json({ error: data.error });
+        return
+    }
     res.json({ message: "User created" });
 })
 
 app.post("/signin", (req, res) => {
-
+    const data = SignInSchema.safeParse(req.body);
+    if (!data.success) {
+        res.status(400).json({ error: data.error });
+        return
+    }
     const userId = 1
     const token = jwt.sign({ userId }, JWT_SECRET);
     res.json({ token });
@@ -18,6 +28,11 @@ app.post("/signin", (req, res) => {
 
 app.post("/room", middleware,(req, res) => {
  
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.status(400).json({ error: data.error });
+        return
+    }
     res.json({ roomId: 123 });
 })
 
