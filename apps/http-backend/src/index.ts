5da0b08403ca71edd0cf1a3834af59d81b5c4ca3
Yash Khare <khareyash05@gmail.com>
2025-04-06 23:46:52 +0530

    feat: signup client
    Signed-off-by: Yash Khare <khareyash05@gmail.com>

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,16 +3,30 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from './middleware';
 import {CreateRoomSchema, CreateUserSchema, SignInSchema} from '@repo/common/types';
+import {primsaClient} from '@repo/db/client';
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
-        res.status(400).json({ error: data.error });
+app.post("/signup", async (req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(400).json({ error: parsedData.error });
         return
     }
-    res.json({ message: "User created" });
+    try{
+        await primsaClient.user.create({
+            data:{
+                email:parsedData.data?.username,
+                name:parsedData.data.name,
+                password:parsedData.data.password,
+            }
+        })
+        res.json({ message: "User created" });
+    }catch(e){
+        console.log(e)
+        res.status(411).json({ error: "User already exists" });
+        return
+    }
 })
 
 app.post("/signin", (req, res) => {
